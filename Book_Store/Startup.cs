74b74c621313094bd99aefef6c934938d3f04708
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-07-12 14:50:57 +0530

    Added : Added : MSMQ Sender

--- Book_Store/Startup.cs
+++ Book_Store/Startup.cs
@@ -87,7 +87,6 @@ namespace Book_Store
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
