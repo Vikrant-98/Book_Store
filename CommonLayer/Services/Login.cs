1b5e435f445be533d49eb4ad4534b37256ce28d7
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-07-13 10:18:30 +0530

    Refactor : Naming Conventions

---
+++ CommonLayer/Services/Login.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CommonLayer.Services
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "Email Is Required")]
+        [RegularExpression(@"^([a-zA-Z0-9]{2}[a-zA-Z0-9]*[.]{0,1}[a-zA-Z0-9]*@[a-zA-Z0-9]*.{1}[a-zA-Z0-9]*[.]*[a-zA-Z0-9]*)$", ErrorMessage = "Enter Valid Email")]
+        //Mail ID
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password Is Required")]
+        [RegularExpression(@"^.{8,15}$", ErrorMessage = "Password Length should be between 8 to 15")]
+        //Password
+        public string Password { get; set; }
+    }
+}
