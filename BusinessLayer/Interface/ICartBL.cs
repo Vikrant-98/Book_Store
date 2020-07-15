8306cbefa3f090205fb7a6107fd9760ecd9c606f
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-07-15 23:04:21 +0530

    Added : Get Add & Delete Cart

---
+++ BusinessLayer/Interface/ICartBL.cs
@@ -0,0 +1,18 @@
+﻿using CommonLayer.Request;
+using CommonLayer.Responce;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Interface
+{
+    public interface ICartBL
+    {
+        Task<CartBookResponse> AddBookIntoCart(int userID, Cart cart);
+
+        Task<List<CartBookResponse>> GetListOfBooksInCart(int userID);
+
+        Task<bool> DeleteBookFromCart(int userID, int cartID);
+    }
+}
---
+++ BusinessLayer/Services/CartBL.cs
@@ -0,0 +1,79 @@
+﻿using BusinessLayer.Interface;
+using CommonLayer.Request;
+using CommonLayer.Responce;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services
+{
+    public class CartBL : ICartBL
+    {
+
+        private readonly ICartRL _cart;
+
+        public CartBL(ICartRL cart)
+        {
+            _cart = cart;
+        }
+
+        public async Task<List<CartBookResponse>> GetListOfBooksInCart(int userID)
+        {
+            try
+            {
+                if (userID <= 0)
+                {
+                    return null;
+                }
+                else
+                {
+                    return await _cart.GetListOfBooksInCart(userID);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<CartBookResponse> AddBookIntoCart(int userID, Cart cart)
+        {
+            try
+            {
+                if (userID <= 0 || cart.BookID == 0)
+                {
+                    return null;
+                }
+                else
+                {
+                    return await _cart.AddBookIntoCart(userID, cart);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<bool> DeleteBookFromCart(int userID, int cartID)
+        {
+            try
+            {
+                if (userID <= 0 || cartID <= 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return await _cart.DeleteBookFromCart(userID, cartID);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
