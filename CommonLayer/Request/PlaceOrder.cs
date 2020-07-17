f7d32898bd86bb56d0aa1363660ae47b4d1259a3
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-07-17 13:58:49 +0530

    Refactor : Module for Cancel Order

---
+++ CommonLayer/Request/PlaceOrder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.Request
+{
+    public class PlaceOrder
+    {
+        public int BookId { get; set; }
+
+        public int CartId { get; set; }
+    }
+    public class CalcelOrder
+    {
+        public int OrderId { get; set; }
+
+        public int BookId { get; set; }
+
+        public int CartId { get; set; }
+    }
+}
---
+++ CommonLayer/Responce/PlaceOrderResponce.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.Responce
+{
+    public class PlaceOrderResponce
+    {
+        public int OrderId { get; set; }
+
+        public int UserId { get; set; }
+
+        public int BookId { get; set; }
+
+        public int CartId { get; set; }
+
+        public string BookName { get; set; }
+
+        public string AuthorName { get; set; }
+
+        public int Price { get; set; }
+
+        public int Pages { get; set; }
+
+        public bool IsPlace { get; set; }
+    }
+}
